1fb51e0cf00affdbfc4a44fbc4ace36a80ef8d2d
jerry-world <sy920112@naver.com>
2025-10-16 10:02:15 +0900

    chore: home API 작성

---
+++ demo/src/main/java/com/example/demo/home/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.home;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/home")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Hello World";
+    }
+}
