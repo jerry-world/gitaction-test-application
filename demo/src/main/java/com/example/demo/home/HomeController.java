b2f35bddb102106cb6decfe10bee3c3081f649f3
jerry-world <sy920112@naver.com>
2025-10-17 18:37:47 +0900

    chore: CHANGES

--- demo/src/main/java/com/example/demo/home/HomeController.java
+++ demo/src/main/java/com/example/demo/home/HomeController.java
@@ -9,6 +9,6 @@ public class HomeController {
     @GetMapping
     public String home() {
         System.out.println("home#3");
-        return "Hello World6";
+        return "Hello World7";
     }
 }
