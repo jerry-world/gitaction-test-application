b7a28ab44de4785bb14ea1f687877c737a369b5d
jerry-world <sy920112@naver.com>
2025-10-18 03:37:50 +0900

    fix: HomeController Requestmapping

--- demo/src/main/java/com/example/demo/home/HomeController.java
+++ demo/src/main/java/com/example/demo/home/HomeController.java
@@ -1,9 +1,11 @@
 package com.example.demo.home;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/home")
+@RestController
+@RequestMapping("/api/v1/home")
 public class HomeController {
 
     @GetMapping
