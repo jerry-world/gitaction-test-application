969a6c04b2ff58d18fadc36d038cfdcabeaffb61
jerry-world <sy920112@naver.com>
2025-10-19 20:57:49 +0900

    chore: json 포멧 변환 및 데이터 포멧팅(6)

--- demo/src/main/java/com/example/demo/home/HomeController.java
+++ demo/src/main/java/com/example/demo/home/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
     @RequestMapping(value = "/home")
     public String home() {
         System.out.println("home Check");
-        System.out.println("Trivy 테스트3");
+        System.out.println("Trivy 테스트4");
         return "Welcome home";
     }
 }
--- demo1/src/main/java/com/example/demo/home1/Home1Controller.java
+++ demo1/src/main/java/com/example/demo/home1/Home1Controller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Home1Controller {
     @GetMapping
     public String home1(){
-        System.out.println("home1_3");
+        System.out.println("home1_4");
         return "home1";
     }
 }
